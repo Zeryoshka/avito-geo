26ed2e28525db322e2a30b79c993ab7378c6f8d7
Zergey Loshkarev <saloshkarev@miem.hse.ru>
2021-07-22 22:10:42 +0300

    include base_keys in __init__.py instead of get and set

--- serdis-server/handlers/__init__.py
+++ serdis-server/handlers/__init__.py
@@ -6,7 +6,6 @@ from typing import Any, Dict, Tuple
 from flask import request
 
 
-from .set import set_handler
-from .get import get_handler
+from base_keys import set_handler, get_handler
 from .list_requests import lget_handler, lset_handler
 from .ping import ping_handler
