c80fe39229c90e938d3ed734af6cd3caa9b4dde3
Zergey Loshkarev <saloshkarev@miem.hse.ru>
2021-08-02 18:36:04 +0300

    delete base of cli __main__.py and utils.py

--- serdis_client/utils.py
+++ serdis_client/utils.py
@@ -2,8 +2,6 @@
 Some specific functions for serdis_client
 '''
 import re
-from typing import List
-from pyparsing import Suppress, Word, ZeroOrMore, alphas, Optional
 
 def is_valid_key(key: str):
     '''
@@ -13,9 +11,3 @@ def is_valid_key(key: str):
     return re.fullmatch(r'[a-zA-Z_][a-zA-Z0-9_]*', key) is not None
 
 
-template = Suppress('[') + Suppress('"') + ZeroOrMore(Word(alphas+'_+')+Suppress('"')+Suppress(',')) + Optional(Word(alphas)) + Suppress(']')
-def list_parse(line: str) -> (List[str]):
-    '''
-    Function for parse list from console to python list
-    '''
-    
